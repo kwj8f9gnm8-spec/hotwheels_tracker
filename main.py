c5ce167288410073463514f40536189b82b306bb
kwj8f9gnm8-spec <kwj8f9gnm8@privaterelay.appleid.com>
2026-06-04 14:59:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from threading import Thread
 from flask import Flask
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 BOT_TOKEN = "8728455127:AAGH3v79KQQGIurSHkVzgDr9szr88PzD-W4"
 CHAT_ID = "5097274608"
