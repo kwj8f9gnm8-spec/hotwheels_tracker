f0c318d19a29b7773f8d7c0e902ac4fea84c085b
kwj8f9gnm8-spec <kwj8f9gnm8@privaterelay.appleid.com>
2026-06-04 12:49:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
+import time
 
 BOT_TOKEN = "YOUR_BOT_TOKEN"
 CHAT_ID = "5097274608"
+
+URL = "https://www.firstcry.com/Hot%20Wheels/0/0/113?q=as_hot+whee&asid=48299"
+
+last_found = False
 
 def send_message(text):
     requests.post(
@@ -12,4 +17,21 @@ def send_message(text):
         }
     )
 
-send_message("🚗 Hot Wheels Tracker Started!")
+while True:
+    try:
+        page = requests.get(URL, timeout=20).text.lower()
+
+        found = (
+            "die cast toy car" in page or
+            "hot wheels" in page
+        )
+
+        if found and not last_found:
+            send_message("🚗 Hot Wheels found on FirstCry!")
+
+        last_found = found
+
+    except Exception as e:
+        print(e)
+
+    time.sleep(300)
